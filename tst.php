ec732e376c12aaa938b898f91fcbe9ff9ed102f2
Manuella <tsoumoumanuellapro@gmail.com>
2026-03-04 21:57:15 +0100

    J'AI REUSSI

--- tst.php
+++ tst.php
@@ -1,1 +1,8 @@
-<?php echo "Le serveur fonctionne !"; ?>
+<?php
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=regis;charset=utf8mb4', 'regis', 'password');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion OK !";
+} catch(PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
